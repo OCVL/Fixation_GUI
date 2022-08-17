d09308708b5480baf3e428b4fcc89cde39a21a98
Jenna Grieshop <jengriesh@gmail.com>
2022-08-17 15:54:28 -0500

    Cleaned up nuclear_notes
    Some comments added, cleaned up file/reorganized lines.

--- ocvl/fixation/nuclear_notes.py
+++ ocvl/fixation/nuclear_notes.py
@@ -8,39 +8,47 @@ class NuclearNotes(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        # read in the config file
         config = configparser.ConfigParser()
         config.read('C:\\Users\\6794grieshj\\Documents\\GitHub\\Fixation_GUI_Qt\\test_settings.ini')
-        # https://stackoverflow.com/questions/4097139/reading-array-from-config-file-in-python
-        horizontal_table_headers = config.get("test", "horizontal_table_headers").split("/")
-
-        self.button = QtWidgets.QPushButton("FakeRecordButton")
 
         self.layout = QtWidgets.QVBoxLayout(self)
+
+        # fake record button to test adding more rows when a video is recorded
+        self.button = QtWidgets.QPushButton("FakeRecordButton")
         self.layout.addWidget(self.button)
         self.button.clicked.connect(self.addRow)
-        self.table_widget = self.constructTable(horizontal_table_headers)
-        self.table_widget.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
-        self.table_widget.setAlternatingRowColors(True)
-        self.layout.addWidget(self.table_widget, stretch=True)
+
         # https: // stackoverflow.com / questions / 54612127 / how - to - i - set - the - size - hint -
         # for -a - qtablewidget - in -python
+        # make the table and set it up to be formatted nicely
+        self.table_widget = self.constructTable(config)
+        self.table_widget.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.table_widget.setAlternatingRowColors(True)
         self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.layout.addWidget(self.table_widget, stretch=True)
 
-    def constructTable(self, headers):
-        table_columns = len(headers)
-        # self.column_width = round(766/self.table_columns)
-        table_rows = 0  # should make this a variable that grows
+    def constructTable(self, config):
+        # https://stackoverflow.com/questions/4097139/reading-array-from-config-file-in-python
+        horizontal_table_headers = config.get("test", "horizontal_table_headers").split("/")
+        table_columns = len(horizontal_table_headers)
+        table_rows = 0
 
         table = QTableWidget()
 
+        # set up rows
         table.setRowCount(table_rows)
         # https: // stackoverflow.com / questions / 578371 / hiding - row - labels
         table.verticalHeader().setVisible(False)
+
+        # set up columns
         table.setColumnCount(table_columns)
         table.setHorizontalHeaderLabels(
-            headers)  # should probably source these from init/preferences file
+            horizontal_table_headers)  # should probably source these from init/preferences file
 
+        # delegate will be used to disable editing of columns
         delegate = ReadOnlyDelegate(self)
+        # these columns will not be editable -- could be pulled from config file
         table.setItemDelegateForColumn(0, delegate)
         table.setItemDelegateForColumn(1, delegate)
         table.setItemDelegateForColumn(2, delegate)
