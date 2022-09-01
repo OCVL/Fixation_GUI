7844db7d330d9239b2433d380277a7f92cdf7b48
Brea <Brea.Brennan@marquette.edu>
2022-09-01 10:43:04 -0500

    Changed Target shape buttons from Radio to push along with setting the size of the buttons to be 100x100 pixels.

--- ocvl/fixation/nuclear_controls.py
+++ ocvl/fixation/nuclear_controls.py
@@ -1,7 +1,7 @@
 from PySide6 import QtCore, QtWidgets, QtGui
 import PySide6.QtCore
 import sys
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QSize
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QTabWidget, QWidget, QFormLayout, QLineEdit, \
     QHBoxLayout, QRadioButton, QSlider, QAbstractSlider, QPushButton, QColorDialog, QVBoxLayout, QGraphicsColorizeEffect
 
@@ -81,32 +81,41 @@ class Tabs(QTabWidget):
         color_button.clicked.connect(self.onpresscolor)
 
         # Radio Buttons to be used for target shape
-        cross = QRadioButton("Large Crosshair")
-        s_cross = QRadioButton("Small Crosshair")
-        m_cross = QRadioButton("Maltese Cross")
-        square_out = QRadioButton("Square Outline")
-        square = QRadioButton("Square")
-        circle = QRadioButton("Circle")
-        twinkle = QRadioButton("Twinkle")
+        self.cross = QPushButton("Large Crosshair")
+        self.s_cross = QPushButton("Small Crosshair")
+        self.m_cross = QPushButton("Maltese Cross")
+        self.square_out = QPushButton("Square Outline")
+        self.square = QPushButton("Square")
+        self.circle = QPushButton("Circle")
+        self.twinkle = QPushButton("Twinkle")
         self.test_label = QLabel("")
 
+        # Change the shape of the buttons to be squares
+        self.cross.setFixedSize(QSize(100, 100))
+        self.s_cross.setFixedSize(QSize(100, 100))
+        self.m_cross.setFixedSize(QSize(100, 100))
+        self.square_out.setFixedSize(QSize(100, 100))
+        self.square.setFixedSize(QSize(100, 100))
+        self.circle.setFixedSize(QSize(100, 100))
+        self.twinkle.setFixedSize(QSize(100, 100))
+
         # Adding the radio buttons to the shape widget
-        fix_shape.addWidget(cross)
-        fix_shape.addWidget(s_cross)
-        fix_shape.addWidget(m_cross)
-        fix_shape.addWidget(square_out)
-        fix_shape.addWidget(square)
-        fix_shape.addWidget(circle)
-        fix_shape.addWidget(twinkle)
+        fix_shape.addWidget(self.cross)
+        fix_shape.addWidget(self.s_cross)
+        fix_shape.addWidget(self.m_cross)
+        fix_shape.addWidget(self.square_out)
+        fix_shape.addWidget(self.square)
+        fix_shape.addWidget(self.circle)
+        fix_shape.addWidget(self.twinkle)
 
         # What will happen when a specific radio button is called
-        cross.toggled.connect(self.onclick)
-        s_cross.toggled.connect(self.onclick)
-        m_cross.toggled.connect(self.onclick)
-        square_out.toggled.connect(self.onclick)
-        square.toggled.connect(self.onclick)
-        circle.toggled.connect(self.onclick)
-        twinkle.toggled.connect(self.onclick)
+        self.cross.clicked.connect(self.onclick)
+        self.s_cross.clicked.connect(self.onclick)
+        self.m_cross.clicked.connect(self.onclick)
+        self.square_out.clicked.connect(self.onclick)
+        self.square.clicked.connect(self.onclick)
+        self.circle.clicked.connect(self.onclick)
+        self.twinkle.clicked.connect(self.onclick)
 
         # Generate the scroll bar for the size of the fixation target
         fix_size = QHBoxLayout()
@@ -141,9 +150,29 @@ class Tabs(QTabWidget):
     # Slot for the shape of the fixation target being completed
     def onclick(self):
         button = self.sender()
-        if button.isChecked():
-            # Will be changed to what each shape will look like in the future
-            self.test_label.setText("You pressed the button called: " + button.text())
+        txt = str(button.text())
+        match txt:
+            case "Large Crosshair":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Small Crosshair":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Maltese Cross":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Square Outline":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Square":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Circle":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
+            case "Twinkle":
+                # Will be changed to what each shape will look like in the future
+                self.test_label.setText("You pressed the button called: " + txt)
 
     # Slot for displaying the value of the fixation target as it moves
     def sizechange(self):
@@ -243,6 +272,11 @@ class Tabs(QTabWidget):
         layout5.addRow("Help:", QLineEdit())
         self.setTabText(4, "Help")
         self.tab5.setLayout(layout5)
+
+    def PaintEvent(self, event):
+            painter = QPainter(self)
+            painter.setPen(QPen(Qt.green, 8, Qt.DashLine))
+            painter.drawEllipse(40, 40, 400, 400)
 
 
 if __name__ == "__main__":
