37fac17344ac249bdfec83bbc20e165b66a24385
Jenna Grieshop <jengriesh@gmail.com>
2022-09-15 11:04:28 -0500

    Lines and Ellipse Updated
    Changed colors and thickness of the lines. Changed the ellipse to be an arc so it doesn't get filled in. Also changed it so the circle stays 15x15 degrees since it is in reference to the fixation target limits.

--- ocvl/fixation/nuclear_panel.py
+++ ocvl/fixation/nuclear_panel.py
@@ -3,7 +3,7 @@ import math
 
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import QPoint
-from PySide6.QtGui import QPainter, Qt
+from PySide6.QtGui import QPainter, Qt, QPen, QColor
 from PySide6.QtWidgets import QWidget
 import numpy as np
 from tkinter import filedialog
@@ -46,7 +46,7 @@ class TargetArea(QWidget):
     def paintEvent(self, arg__0):
 
         painter = QPainter(self)
-        painter.setBrush(Qt.darkGray)
+        painter.setBrush(QColor(75, 75, 75))
         painter.setRenderHint(QPainter.Antialiasing, True)
 
         rect = painter.window()
@@ -62,21 +62,34 @@ class TargetArea(QWidget):
         else:
             num_of_lines = 0
 
+        spacing = (radii*2)/num_of_lines
         painter.drawRect(rect.width() / 2 - radii, rect.height() / 2 - radii, radii * 2, radii * 2)
-        if self.circle_vis == "1":
-            painter.setPen(Qt.blue)
-            painter.drawEllipse(cent, radii, radii)
-        else:
-            pass
-        painter.setPen(Qt.black)
 
         horz_steps = np.linspace(rect.width()/2-radii, rect.width()/2+radii, num_of_lines)
         vert_steps = np.linspace(rect.height()/2-radii, rect.height()/2+radii, num_of_lines)
 
+        center_line = (num_of_lines-1)/2
+
+        counter = 0
+        for y in vert_steps:
+            if counter % 5 == 0:
+                if counter == center_line:
+                    painter.setPen(QPen(QColor(255, 79, 0), 2.5))
+                else:
+                    painter.setPen(QPen(QColor(255, 79, 0)))
+                painter.drawLine(rect.width() / 2 - radii, y, rect.width() / 2 + radii, y)
+            else:
+                painter.setPen(Qt.black)
+                painter.drawLine(rect.width() / 2 - radii, y, rect.width() / 2 + radii, y)
+            counter += 1
+
         counter = 0
         for x in horz_steps:
             if counter % 5 == 0:
-                painter.setPen(Qt.red)
+                if counter == center_line:
+                    painter.setPen(QPen(QColor(255, 79, 0), 2.5))
+                else:
+                    painter.setPen(QPen(QColor(255, 79, 0)))
                 painter.drawLine(x, rect.height() / 2 - radii, x, rect.height() / 2 + radii)
             else:
                 painter.setPen(Qt.black)
@@ -85,16 +98,13 @@ class TargetArea(QWidget):
 
         painter.setPen(Qt.black)
 
-        counter = 0
-        for y in vert_steps:
-            if counter % 5 == 0:
-                painter.setPen(Qt.red)
-                painter.drawLine(rect.width() / 2 - radii, y, rect.width() / 2 + radii, y)
-            else:
-                painter.setPen(Qt.black)
-                painter.drawLine(rect.width()/2-radii, y, rect.width()/2+radii, y)
-            counter += 1
-
+        if self.circle_vis == "1":
+            painter.setPen(QPen(QColor(3, 175, 224), 2.5))
+            # painter.drawEllipse(cent, spacing * 15.5, spacing * 15.5)
+            painter.drawArc((rect.width()/2)-(spacing * 15.25), (rect.height()/2)-(spacing * 15.25), spacing * 30.5, spacing * 30.5, 0, 16*360)
+            print('')
+        else:
+            pass
         painter.setPen(Qt.black)
 
 
