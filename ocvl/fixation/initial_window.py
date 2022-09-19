8d531398a78533565dc7668133fcf50db0335eff
Jenna Grieshop <jengriesh@gmail.com>
2022-09-19 09:47:30 -0500

    Moved initial window code to new file
    New python file to hold the class InitialDialog

---
+++ ocvl/fixation/initial_window.py
@@ -0,0 +1,86 @@
+import configparser
+from tkinter import filedialog
+
+from PySide6.QtWidgets import QDialog, QLabel, QPushButton, QLineEdit, QComboBox, QDialogButtonBox, QFormLayout, \
+    QHBoxLayout, QRadioButton
+
+
+class InitialDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.temp_loc_name = QLabel("")
+        self.save_location_butt = QPushButton("Save Location")
+        self.sub_id = QLineEdit()
+        self.device_menu = QComboBox()
+        self.save_location_dir = None
+        self.device_selected = None
+        self.buttonBox = None
+        self.setWindowTitle("Setup")
+        self.config = configparser.ConfigParser()
+        self.config_name = filedialog.askopenfilenames(title='Select the configuration file', filetypes=[
+            ("configuration file", ".ini")])
+        self.config.read(self.config_name)
+        self.device_list = self.config.get("ALL", "device_list").split("/")
+        self.setup()
+
+    def setup(self):
+        # Button and connection code to exit pop up when done
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        # Sets up the layout for the pop up window
+        layout1 = QFormLayout()
+        eye_butt_layout = QHBoxLayout()
+        save_loc_butt_layout = QHBoxLayout()
+
+        # Eye Radio button generation and slot/signal linking
+        left_eye = QRadioButton("OS")
+        right_eye = QRadioButton("OD")
+        left_eye.toggled.connect(self.eye_slot)
+        right_eye.toggled.connect(self.eye_slot)
+
+        # Linking the field where the sub id is listed to be saved for later use
+        self.sub_id.textChanged.connect(self.onTextEnter)
+
+        # Linking the button used to get the save direction and the variable to store this for later use
+        self.save_location_butt.clicked.connect(self.on_save_click)
+
+        # Generating and linking the drop down menu to the signal/slot to save device selected
+        self.device_menu.addItem("Select Device")
+        for x in self.device_list:
+            self.device_menu.addItem(x)
+
+        self.device_menu.currentTextChanged.connect(self.on_combobox_changed)
+
+        # Adding the sub widgets to their boxes for display purposes
+        save_loc_butt_layout.addWidget(self.save_location_butt)
+        eye_butt_layout.addWidget(left_eye)
+        eye_butt_layout.addWidget(right_eye)
+
+        # Adding all components to the main layout of the pop up window
+        layout1.addRow("Select Eye", eye_butt_layout)
+        layout1.addRow("Subject ID:", self.sub_id)
+        layout1.addRow("Select Save Location", save_loc_butt_layout)
+        layout1.addRow(self.temp_loc_name)
+        layout1.addRow("Device", self.device_menu)
+        layout1.addWidget(self.buttonBox)
+
+        self.setLayout(layout1)
+
+    def eye_slot(self):
+        button = self.sender()
+        if button.isChecked():
+            print("Pressed the button called: " + button.text())
+
+    def onTextEnter(self):
+        print(self.sub_id.text())
+
+    def on_save_click(self):
+        self.save_location_dir = filedialog.askdirectory(title='Select the save location for generated files.')
+        self.temp_loc_name.setText(self.save_location_dir)
+
+    def on_combobox_changed(self):
+        self.device_selected = self.device_menu.currentText()
+        print(self.device_selected)
--- ocvl/fixation/nuclear_panel.py
+++ ocvl/fixation/nuclear_panel.py
@@ -1,24 +1,23 @@
 import configparser
-import math
 
 from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import QPoint, QRect
 from PySide6.QtGui import QPainter, Qt, QPen, QColor
-from PySide6.QtWidgets import QWidget, QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QFormLayout, QLineEdit, \
-    QPushButton, QHBoxLayout, QRadioButton, QComboBox
+from PySide6.QtWidgets import QWidget
 import numpy as np
-from tkinter import filedialog
+from ocvl.fixation.initial_window import InitialDialog
 
 
 class NuclearDisplay(QWidget):
     def __init__(self):
         super().__init__()
 
-        dlg = CustomDialog()
+        dlg = InitialDialog()
         dlg.exec()
-        c_name = dlg.config_name # config file name
-        d_name = dlg.device_selected # device name
-        self.target_area = TargetArea(c_name)
+        config_name = dlg.config_name  # config file name
+        dev_name = dlg.device_selected  # device name
+        save_loc_dir = dlg.save_location_dir  # save location directory path
+        self.target_area = TargetArea(config_name)
         self.lefty = TargetLefty()
         self.righty = TargetRighty()
 
@@ -34,87 +33,6 @@ class NuclearDisplay(QWidget):
     @QtCore.Slot()
     def gridSizeInDeg(self):
         pass
-
-class CustomDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.temp_loc_name = QLabel("")
-        self.save_location_butt = QPushButton("Save Location")
-        self.sub_id = QLineEdit()
-        self.device_menu = QComboBox()
-        self.save_location_dir = None
-        self.device_selected = None
-        self.buttonBox = None
-        self.setWindowTitle("Setup")
-        self.config = configparser.ConfigParser()
-        self.config_name = filedialog.askopenfilenames(title='Select the configuration file', filetypes=[
-            ("configuration file", ".ini")])
-        self.config.read(self.config_name)
-        self.device_list = self.config.get("ALL", "device_list").split("/")
-        self.setup()
-
-    def setup(self):
-        # Button and connection code to exit pop up when done
-        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
-        self.buttonBox = QDialogButtonBox(QBtn)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        # Sets up the layout for the pop up window
-        layout1 = QFormLayout()
-        eye_butt_layout = QHBoxLayout()
-        save_loc_butt_layout = QHBoxLayout()
-
-        # Eye Radio button generation and slot/signal linking
-        left_eye = QRadioButton("OS")
-        right_eye = QRadioButton("OD")
-        left_eye.toggled.connect(self.eye_slot)
-        right_eye.toggled.connect(self.eye_slot)
-
-        # Linking the field where the sub id is listed to be saved for later use
-        self.sub_id.textChanged.connect(self.onTextEnter)
-
-        # Linking the button used to get the save direction and the variable to store this for later use
-        self.save_location_butt.clicked.connect(self.on_save_click)
-
-        # Generating and linking the drop down menu to the signal/slot to save device selected
-        self.device_menu.addItem("Select Device")
-        for x in self.device_list:
-            self.device_menu.addItem(x)
-
-        self.device_menu.currentTextChanged.connect(self.on_combobox_changed)
-
-        # Adding the sub widgets to their boxes for display purposes
-        save_loc_butt_layout.addWidget(self.save_location_butt)
-        eye_butt_layout.addWidget(left_eye)
-        eye_butt_layout.addWidget(right_eye)
-
-        # Adding all components to the main layout of the pop up window
-        layout1.addRow("Select Eye", eye_butt_layout)
-        layout1.addRow("Subject ID:", self.sub_id)
-        layout1.addRow("Select Save Location", save_loc_butt_layout)
-        layout1.addRow(self.temp_loc_name)
-        layout1.addRow("Device", self.device_menu)
-        layout1.addWidget(self.buttonBox)
-
-        self.setLayout(layout1)
-
-    def eye_slot(self):
-        button = self.sender()
-        if button.isChecked():
-            print("Pressed the button called: " + button.text())
-
-    def onTextEnter(self):
-        print(self.sub_id.text())
-
-    def on_save_click(self):
-        self.save_location_dir = filedialog.askdirectory(title='Select the save location for generated files.')
-        self.temp_loc_name.setText(self.save_location_dir)
-
-    def on_combobox_changed(self):
-        self.device_selected = self.device_menu.currentText()
-        print(self.device_selected)
-
 
 class TargetArea(QWidget):
     def __init__(self, config_name):
